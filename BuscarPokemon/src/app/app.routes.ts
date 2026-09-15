de5545d1008e1504b116259be2e5658502b32376
Axovi.AP <jeanalepacheco10@gmail.com>
2026-09-15 01:53:31 +0000

    PokeApi

--- BuscarPokemon/src/app/app.routes.ts
+++ BuscarPokemon/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
-import { RegistroUsuarioComponent } from './component/registro-usuario/registro-usuario.component';
-import { BuscadorPokemonComponent } from './component/buscador-pokemon/buscador-pokemon.component';
+
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'registro', pathMatch: 'full' },
-  { path: 'registro', component: RegistroUsuarioComponent },
-  { path: 'buscador', component: BuscadorPokemonComponent },
-  { path: '**', redirectTo: 'registro' }
+  { path: '', pathMatch: 'full' },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./component/registro-usuario/registro-usuario.component').then(
+        ({ RegistroUsuarioComponent }) => RegistroUsuarioComponent
+      )
+  },
+  {
+    path: 'buscador',
+    loadComponent: () =>
+      import('./component/buscador-pokemon/buscador-pokemon.component').then(
+        ({ BuscadorPokemonComponent }) => BuscadorPokemonComponent
+      )
+  }
 ];
